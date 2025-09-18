2b5ad9e03e07da543e35564a16239d294b3231ed
MarColnaghi <marc.colnaghi@gmail.com>
2025-09-18 16:21:31 +0100

    thu updates, fixing the frame end initi

--- code/extrema_detection/extract_jumps.m
+++ code/extrema_detection/extract_jumps.m
@@ -15,19 +15,19 @@ gen_data = 'fr';
 % Load important files
 paths = path_generator('folder', fullfile('/fitting_freezes/le', model, select_run));
 load(fullfile(paths.results, 'surrogate.mat'))
-load('/Users/marcocolnaghi/PhD/freeze_ddm/model_results/momentary_integration/surrogate/dddm2/run13/sims_tv/y.mat')
+load('/Users/marcocolnaghi/PhD/freeze_ddm/model_results/momentary_integration/surrogate/dddm2/run16/sims_tv/y.mat')
 
 % Load the motion ts
 sim_params.motion_cache_path = fullfile(paths.dataset, 'motion_cache.mat');
 load(sim_params.motion_cache_path)
 
-d = 240;
-n_selected_comparisons = 100;
+d = 240; percentage_2b_exp = 0.5;
+n_selected_comparisons = 150;
 y = y(y.durations_s > d/60 & y.durations_s <= 10.5, :);
 sm_cell = extract_sm_cellarray(y, motion_cache);
 col = cbrewer2('Spectral', n_selected_comparisons);
 
-for idx_bout = 650:700
+for idx_bout = 421:450
 
     sm = motion_cache(y.fly(idx_bout));
     sm_bout = sm(y.onsets(idx_bout):y.onsets(idx_bout) + round(y.durations_s(idx_bout) .* 60));
@@ -38,27 +38,22 @@ for idx_bout = 650:700
     similarity_sort = nan(1, height(y));
 
     for idx_comparison = 1:height(y)
-        extracted_sm = sm_cell{idx_comparison};
+        comparison_sm = motion_cache(y.fly(idx_comparison));
+        start = y.onsets(idx_comparison);
+        ending = y.onsets(idx_comparison) + round(y.durations_s(idx_comparison) .* 60)  + d * percentage_2b_exp;
+        comparison_sm_cropped = comparison_sm(start:ending);
 
-        if length(extracted_sm) < d
-            similarity_value(idx_comparison) = nan;
-            similarity_sort(idx_comparison) = nan;
-            disp('hello')
-        else
+        similarity_framebframe = nan(1, length(comparison_sm) - d);
 
-            similarity_framebframe = nan(1, length(extracted_sm) - d);
-
-            for idx_frame = 1:length(extracted_sm) - d
-                v2 = extracted_sm(idx_frame:idx_frame + d);
-                similarity_framebframe(idx_frame) = norm(v1 - v2);
-            end
-
-            [closest_similarity, best_frame] = min(similarity_framebframe);
-            similarity_value(idx_comparison) = closest_similarity;
-            similarity_sort(idx_comparison) = best_frame;
-
+        for idx_frame = 1:length(comparison_sm_cropped) - d
+            v2 = comparison_sm_cropped(idx_frame:idx_frame + d);
+            similarity_framebframe(idx_frame) = norm(v1 - v2);
         end
 
+        [closest_similarity, best_frame] = min(similarity_framebframe);
+        similarity_value(idx_comparison) = closest_similarity;
+        similarity_sort(idx_comparison) = best_frame;
+        all_cropped{idx_comparison} = comparison_sm_cropped;
     end
 
     [sorted_similarity_magnitude, sorted_similarity_idx] = sort(similarity_value);
@@ -68,10 +63,10 @@ for idx_bout = 650:700
     tiledlayout(2, 3, 'TileSpacing', 'loose', 'Padding', 'loose')
     nexttile(1)
     hold on
-    histogram(similarity_value, 'EdgeColor', 'none' )
-    xline(prctile(similarity_value, 10), 'r--')
+    histogram(sorted_similarity_magnitude, 25, 'EdgeColor', 'none' )
+    xline(prctile(sorted_similarity_magnitude, 10), 'r--')
     ylabel('Count')
-    ylabel('Social Motion')
+    xlabel('Social Motion')
 
     apply_generic(gca, 20)
     nexttile(2, [1 2])
@@ -79,27 +74,32 @@ for idx_bout = 650:700
 
 
     for idx_tops = 1:length(best_similarities)
-        extracted_sm = sm_cell{best_similarities(idx_tops)};
 
+        comparison_sm_cropped = all_cropped{best_similarities(idx_tops)};
 
-        init = similarity_sort(best_similarities(idx_tops));
-        v2 = extracted_sm(similarity_sort(best_similarities(idx_tops)):similarity_sort(best_similarities(idx_tops)) + d);
-        initial_bump(idx_bout, idx_tops) = sum(extracted_sm(1:similarity_sort(best_similarities(idx_tops))));
-        ending_duration(idx_bout, idx_tops) = length(extracted_sm(similarity_sort(best_similarities(idx_tops)):end)) - d - 1;
+        starting_frame = similarity_sort(best_similarities(idx_tops));
+        
+        initial_bump(idx_bout, idx_tops) = sum(comparison_sm_cropped(1:starting_frame - 1));
 
-        %plot(-init + 1: - init + length(extracted_sm), extracted_sm, 'Color', col(idx_tops, :))
+        ending_duration(idx_bout, idx_tops) = length(comparison_sm_cropped(1 : round(y.durations_s(best_similarities(idx_tops)) .* 60))) - length(comparison_sm_cropped(1:starting_frame - 1));
+
+        %plot(-starting_frame + 1: - starting_frame + length(comparison_sm), comparison_sm, 'Color', col(idx_tops, :))
 
     end
 
     [init_bump_sorted, init_bump_idx] = sort(initial_bump(idx_bout, :), 'descend');
 
     i = 0;
+    
     for idx_tops = init_bump_idx
+        
         i = i + 1;
-        extracted_sm = sm_cell{best_similarities(idx_tops)};
-        init = similarity_sort(best_similarities(idx_tops));
-        v2 = extracted_sm(similarity_sort(best_similarities(idx_tops)):similarity_sort(best_similarities(idx_tops)) + d);
-        plot(-init + 1: - init + length(extracted_sm), extracted_sm, 'Color', col(i, :))
+
+        comparison_sm_cropped = all_cropped{best_similarities(idx_tops)};
+        
+        starting_frame = similarity_sort(best_similarities(idx_tops));
+        
+        plot(-starting_frame + 1: - starting_frame + length(comparison_sm_cropped), comparison_sm_cropped, 'Color', col(i, :))
     end
 
     plot(1:length(v1), v1, 'k--', 'LineWidth', 3)
@@ -116,7 +116,7 @@ for idx_bout = 650:700
     yline(y.durations_s(idx_bout), 'k--', 'LineWidth', 2)
     xline(initial_bump(idx_bout, 1), 'k--', 'LineWidth', 2)
     apply_generic(gca,20)
-    xlim([-2 202])
+    xlim([-5 205])
     ylim([-0.5 11.5])
 
     nexttile
@@ -129,7 +129,7 @@ for idx_bout = 650:700
     xline(initial_bump(idx_bout, 1) , 'k--', 'LineWidth', 2)
     apply_generic(gca,20)
     ylim([-0.5 11.5])
-    xlim([-2 202])
+    xlim([-5 205])
 
 end
 
