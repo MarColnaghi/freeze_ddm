dcf6ac878b91429bc39d344da034b02883467f2e
MarColnaghi <marc.colnaghi@gmail.com>
2025-09-20 16:13:29 +0100

    small upd

--- code/extrema_detection/extract_jumps.m
+++ code/extrema_detection/extract_jumps.m
@@ -1,35 +1,130 @@
-clear all
+clearvars
 close all
 
-exporting = true;
-col = cmapper();
+exporting = false;
 
 % Model
 model = 'dddm2';
 select_run = 'run03';
 gen_data = 'fr';
 
-% Load important files
+% Specify Paths
 paths = path_generator('folder', fullfile('/fitting_freezes/le', model, select_run));
-load(fullfile(paths.results, 'surrogate.mat'))
-%load('/Users/marcocolnaghi/PhD/freeze_ddm/model_results/momentary_integration/surrogate/dddm2/run11/sims_tv/y.mat')
-y = surrogate;
 
 % Load the motion ts
 sim_params.motion_cache_path = fullfile(paths.dataset, 'motion_cache.mat');
 load(sim_params.motion_cache_path)
 
 % Parameters
-d = 60; percentage_2b_exp = 1/2; extra_frames = round(percentage_2b_exp * d);
+d = 180; L = d + 1;
+percentage_2b_exp = 1 - 1/3; 
 n_selected_comparisons = 120;
-y = y(y.durations_s > d/60 & y.durations_s <= 10.5, :);
-sm_cell = extract_sm_cellarray(y, motion_cache);
+
+% Calculations
+extra_frames = round(percentage_2b_exp * d) - 1;
+
+% Select Color Map
 col = cbrewer2('Spectral', n_selected_comparisons);
 
-% Preallocate once before the outer loop
-initial_bump    = nan(height(y), n_selected_comparisons);
-ending_duration = nan(height(y), n_selected_comparisons);
-corr_array      = nan(height(y), 1);
+fh = figure('color','w','Position',[100, 100, 600, 250]);
+hold on
+
+for idx_surrogate_run = 14
+    
+    clear y
+    load(sprintf('/Users/marcocolnaghi/PhD/freeze_ddm/model_results/momentary_integration/surrogate/dddm2/run%d/sims_tv/y.mat', idx_surrogate_run))
+
+    % Select only freezes with specific durations
+    y = y(y.durations_s > d/60 & y.durations_s <= 10.5, :);
+
+    flies = y.fly;
+    allfr_onsets = y.onsets;
+    allfr_durations = round(y.durations_s .* 60);
+    allfr_offsets = allfr_onsets + allfr_durations;
+
+    % Preallocate once before the outer loop
+    initial_bump    = nan(height(y), n_selected_comparisons);
+    ending_duration = nan(height(y), n_selected_comparisons);
+    ending_duration_sorted = nan(height(y), n_selected_comparisons);
+    
+    corr_array_surr = nan(height(y), 1);
+
+    for idx_bout = 1:height(y)
+
+        fprintf('running bout #%d out of #%d \n', idx_bout, height(y))
+
+        sm = motion_cache(flies(idx_bout));
+        freeze_onset = allfr_onsets(idx_bout);
+        freeze_duration = allfr_durations(idx_bout);
+
+        sm_bout = sm(freeze_onset:freeze_onset + freeze_duration);
+
+        template_onset = length(sm_bout) - d + 1;
+        
+        v1 = sm_bout(template_onset:end);
+        
+        similarity_value = nan(1, height(y));
+        similarity_sort = nan(1, height(y));
+        
+        all_cropped = cell(1, height(y));
+
+        for idx_comparison = 1:height(y)
+            
+            comparison_sm = motion_cache(flies(idx_comparison));
+
+            start = allfr_onsets(idx_comparison);
+            ending = allfr_offsets(idx_comparison) + extra_frames;
+            comparison_sm_cropped = comparison_sm(start:ending);
+
+            similarity_framebframe = nan(1, length(comparison_sm_cropped) - d + 1);
+
+           
+            dots = conv(comparison_sm_cropped, flipud(v1), 'valid');
+            ssq  = conv(comparison_sm_cropped.^2, ones(d, 1), 'valid');
+            v1sq = sum(v1.^2);
+           
+            similarity_framebframe_vectorized = sqrt(max(v1sq + ssq - 2*dots, 0));
+
+            [closest_similarity, best_frame] = min(similarity_framebframe_vectorized);
+            similarity_value(idx_comparison) = closest_similarity;
+            similarity_sort(idx_comparison) = best_frame;
+            all_cropped{idx_comparison} = comparison_sm_cropped;
+
+        end
+
+        [sorted_similarity_magnitude, best_similarities] = mink(similarity_value, n_selected_comparisons);
+
+        for idx_tops = 1:length(best_similarities)
+
+            comparison_sm_cropped = all_cropped{best_similarities(idx_tops)};
+
+            starting_frame = similarity_sort(best_similarities(idx_tops));
+
+            initial_bump(idx_bout, idx_tops) = sum(comparison_sm_cropped(1:starting_frame - 1));
+
+            ending_duration(idx_bout, idx_tops) = length(comparison_sm_cropped(1 : round(y.durations_s(best_similarities(idx_tops)) .* 60))) - length(comparison_sm_cropped(1:starting_frame - 1));
+
+        end
+
+        [init_bump_sorted, init_bump_idx] = sort(initial_bump(idx_bout, :), 'descend');
+
+        corr_bout = corrcoef(init_bump_sorted, ending_duration(idx_bout, init_bump_idx) ./60);
+
+        ending_duration_sorted(idx_bout, :) =  ending_duration(idx_bout, init_bump_idx);
+
+        corr_array_surr(idx_bout) = corr_bout(1,2);
+    end
+    
+    histogram(corr_array_surr, -1:0.025:1, 'Normalization', 'pdf')
+
+end
+
+apply_generic(gca)
+
+
+
+
+%%
 
 
 for idx_bout = 1:height(y)
@@ -37,8 +132,8 @@ for idx_bout = 1:height(y)
     sm = motion_cache(y.fly(idx_bout));
     sm_bout = sm(y.onsets(idx_bout):y.onsets(idx_bout) + round(y.durations_s(idx_bout) .* 60));
     
-    offset = length(sm_bout); onset = offset - d;
-    v1 = sm_bout(onset:offset);
+    offset = length(sm_bout); template_onset = offset - d;
+    v1 = sm_bout(template_onset:offset);
     similarity_value = nan(1, height(y));
     similarity_sort = nan(1, height(y));
     all_cropped = cell(1, height(y));
@@ -81,24 +176,47 @@ for idx_bout = 1:height(y)
     [init_bump_sorted, init_bump_idx] = sort(initial_bump(idx_bout, :), 'descend');
   
     corr_bout = corrcoef(init_bump_sorted, ending_duration(idx_bout, init_bump_idx) ./60);
-    corr_array(idx_bout) = corr_bout(1,2);
+    corr_array_emp(idx_bout) = corr_bout(1,2);
 end
 
 
 fh = figure('color','w','Position',[100, 100, 600, 250]);
-histogram(corr_array, -1:0.025:1)
+histogram(corr_array_emp, -1:0.02:1)
+hold on
 apply_generic(gca)
 
 %%
-[best_corr, best_corr_idx] = sort(corr_array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+[best_corr, best_corr_idx] = sort(corr_array_emp);
 
 for idx_bout = [best_corr_idx(end-20:end)]'
 
     sm = motion_cache(y.fly(idx_bout));
     sm_bout = sm(y.onsets(idx_bout):y.onsets(idx_bout) + round(y.durations_s(idx_bout) .* 60));
     
-    offset = length(sm_bout); onset = offset - d;
-    v1 = sm_bout(onset:offset);
+    offset = length(sm_bout); template_onset = offset - d;
+    v1 = sm_bout(template_onset:offset);
     similarity_value = nan(1, height(y));
     similarity_sort = nan(1, height(y));
 
@@ -195,5 +313,6 @@ for idx_bout = [best_corr_idx(end-20:end)]'
     xlim([-5 205])
 
     corr_bout = corrcoef(init_bump_sorted, ending_duration(idx_bout, init_bump_idx) ./60);
-    corr_array(idx_bout) = corr_bout(2);
+    corr_array_emp(idx_bout) = corr_bout(2);
 end
+
